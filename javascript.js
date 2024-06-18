e4bc74d2c2a75f73bdcebc95623ca973903ce7f6
S-PetersNZ <137010917+S-PetersNZ@users.noreply.github.com>
2024-06-18 17:00:59 +1200

    Work towards updating for rock-paper-scissors UI Added style.css

--- javascript.js
+++ javascript.js
@@ -1,3 +1,16 @@
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+const buttons = document.querySelectorAll("button");
+
+//Event listener for starting a game
+buttons.forEach((button) => {
+    button.addEventListener("click", function (e) {
+        const humanFirstChoice = e.target.id;
+        playGame(5, humanFirstChoice);
+    });
+});
+
 let humanScore = 0;
 let computerScore = 0;
 
@@ -16,18 +29,38 @@ function getComputerChoice() {
         computerChoice = "scissors";
    }
    return computerChoice;
-}
+};
 
+//Event listener for continuing to play the game once started
 function getHumanChoice() {
-    const humanChoice = prompt("Enter 'Rock, Paper, or Scissors'");
-    return humanChoice;
-}
+    rockButton.addEventListener("click", function (e) {
+        humanSelection = e.target.id;
+    });
+    paperButton.addEventListener("click", function (e) {
+        humanSelection = e.target.id;
+    });
+    scissorsButton.addEventListener("click", function (e) {
+        humanSelection = e.target.id;
+    });
+    return humanSelection;
+};
 
-function playGame(numOfRounds) {
-    for (let i = 0; i < numOfRounds; i++) {
-        const humanSelection = getHumanChoice();
+function playGame(numOfRounds, humanFirstChoice) {
+    if (typeof i === 'undefined') {
+        const humanSelection = humanFirstChoice;
         const computerSelection = getComputerChoice();
         playRound(computerSelection, humanSelection);
+        let i = 1;
+        console.log(i);
+        //i ALWAYS EQUALS 1 SO THIS LOOP KEEPS GOING AROUND AND IT DOESN'T
+        //MOVE TO NEXT LOOP
+        //NEED TO NEST SCORE COMPARISONS WITHIN LAST LOOP?
+    } else {
+        for (let i = 1; i < numOfRounds; i++) {
+            const humanSelection = getHumanChoice();
+            const computerSelection = getComputerChoice();
+            playRound(computerSelection, humanSelection);
+        }
     }
     if (humanScore == computerScore) {
         console.log("It's a tie for the game! The score was " + 
@@ -39,14 +72,13 @@ function playGame(numOfRounds) {
         console.log("You lost the game! The score was " + 
         computerScore + " to " + humanScore);
     }
-}
+};
 
 function playRound(computerSelection, humanSelection) {
-    const humanSelectionSanitized = humanSelection.toLowerCase();
-    if (humanSelectionSanitized == computerSelection) {
+    if (humanSelection == computerSelection) {
         console.log("Both players have chosen " + 
         computerSelection + "! It's a tie!");
-    } else if (humanSelectionSanitized == "rock") {
+    } else if (humanSelection == "rock") {
         if (computerSelection == "paper") {
             console.log("You lose! " + computerSelection + " beats " + 
             humanSelection);
@@ -56,7 +88,7 @@ function playRound(computerSelection, humanSelection) {
             computerSelection);
             humanScore++;
         }
-    } else if (humanSelectionSanitized == "paper") {
+    } else if (humanSelection == "paper") {
         if (computerSelection == "scissors") {
             console.log("You lose! " + computerSelection + " beats " + 
             humanSelection);
@@ -66,7 +98,7 @@ function playRound(computerSelection, humanSelection) {
             computerSelection);
             humanScore++;
         }
-    } else if (humanSelectionSanitized == "scissors") {
+    } else if (humanSelection == "scissors") {
         if (computerSelection == "rock") {
             console.log("You lose! " + computerSelection + " beats " + 
             humanSelection);
@@ -78,6 +110,4 @@ function playRound(computerSelection, humanSelection) {
         }
     }
     return humanScore, computerScore;
-}
-
-playGame(5);
+};
