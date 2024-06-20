342e84c65654736ff06f1b5406293e71c5b1cf33
S-PetersNZ <137010917+S-PetersNZ@users.noreply.github.com>
2024-06-20 19:26:56 +1200

    Rework of Javascript to flesh out UI

--- javascript.js
+++ javascript.js
@@ -1,86 +1,97 @@
 const rockButton = document.querySelector("#rock");
 const paperButton = document.querySelector("#paper");
 const scissorsButton = document.querySelector("#scissors");
+const restartButton = document.querySelector("#restartButton");
 const buttons = document.querySelectorAll("button");
+const endGameModal = document.getElementById("endGameModal");
 
-rockButton.addEventListener("click", () => playRound("rock"));
-paperButton.addEventListener("click", () => playRound("paper"));
-scissorsButton.addEventListener("click", () => playRound("scissors"));
+rockButton.addEventListener("click", () => playRound("Rock"));
+paperButton.addEventListener("click", () => playRound("Paper"));
+scissorsButton.addEventListener("click", () => playRound("Scissors"));
 
-let humanScore = 0;
+//add listener for the close button on the end game modal
+
+let playerScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
+    var computerChoice = "";
     let randomRoll = Math.floor(Math.random() * 4);
     if (randomRoll === 1) {
-        computerChoice = "rock";
+        computerChoice = "Rock";
     } else if (randomRoll === 2) {
-        computerChoice = "paper";
+        computerChoice = "Paper";
     } else {
-        computerChoice = "scissors";
+        computerChoice = "Scissors";
    }
    return computerChoice;
 };
 
-function playRound(humanSelection) {
-    const computerSelection = getComputerChoice();
-    if (humanSelection == computerSelection) {
-        console.log("Both players have chosen " + 
-        computerSelection + "! It's a tie!");
-    } else if (humanSelection == "rock") {
-        if (computerSelection == "paper") {
-            console.log("You lose! " + computerSelection + " beats " + 
-            humanSelection);
+function playRound(playerChoice) {
+    const computerChoice = getComputerChoice();
+    if (playerChoice == computerChoice) {
+        document.getElementById("introTextHeading").innerText = "It's a tie!"
+        document.getElementById("introTextBody").innerText =
+        "Both players have chosen " + playerChoice;
+    } else if (
+            (playerChoice === "Rock") && (computerChoice === "Paper") ||
+            (playerChoice === "Paper") && (computerChoice === "Scissors") ||
+            (playerChoice === "Scissors") && (computerChoice === "Rock")
+        ) {
+            document.getElementById("introTextHeading").innerText = "You lose!";
+            document.getElementById("introTextBody").innerText =
+            computerChoice + " beats " + playerChoice;
             computerScore++;
-        } else {
-            console.log("You win! " + humanSelection + " beats " + 
-            computerSelection);
-            humanScore++;
-        }
-    } else if (humanSelection == "paper") {
-        if (computerSelection == "scissors") {
-            console.log("You lose! " + computerSelection + " beats " + 
-            humanSelection);
-            computerScore++;
-        } else {
-            console.log("You win!", humanSelection, " beats ", 
-            computerSelection);
-            humanScore++;
-        }
-    } else if (humanSelection == "scissors") {
-        if (computerSelection == "rock") {
-            console.log("You lose! " + computerSelection + " beats " + 
-            humanSelection);
-            computerScore++;
-        } else {
-            console.log("You win! " + humanSelection + " beats " + 
-            computerSelection);
-            humanScore++;
-        }
+    } else {
+        document.getElementById("introTextHeading").innerText = "You win!";
+        document.getElementById("introTextBody").innerText =
+        playerChoice + " beats " + computerChoice;
+        playerScore++;
     }
-    console.log("Human score:", humanScore);
-    console.log("Computer score:", computerScore);
-    if ((humanScore === 5) || (computerScore === 5)) {
-        let winner = gameWinner(humanScore, computerScore);
-        return;
+    updateGameScoreBoard(playerScore, computerScore, playerChoice,
+    computerChoice);
+    if ((playerScore === 5) || (computerScore === 5)) {
+        endGameScoreBoard(playerScore, computerScore);
     }
+    return;
 };
 
-function gameWinner(humanScore, computerScore) {
-    if (humanScore == computerScore) {
-        console.log("It's a tie for the game! The score was " + 
-        humanScore + " each");
-    } else if (humanScore > computerScore) {
-        console.log("Congrats! You win the game with a score of " + 
-        humanScore + " to " + computerScore);
+function updateGameScoreBoard(playerScore, computerScore, playerChoice,
+computerChoice) {
+    document.getElementById("playerScore").innerText = playerScore;
+    document.getElementById("computerScore").innerText = computerScore;
+    document.getElementById("playerChoice").innerText = playerChoice;
+    document.getElementById("computerChoice").innerText = computerChoice;
+}
+
+function endGameScoreBoard(playerScore, computerScore) {
+    gameWinnerString = gameWinner(playerScore, computerScore);
+    document.getElementById("winnerDisplay").innerText = gameWinnerString;
+    restartButton.addEventListener("click", () => restartGame());
+    endGameModal.style.display = "block";
+}
+
+function gameWinner(playerScore, computerScore) {
+    let gameWinnerVar = "";
+    if (playerScore > computerScore) {
+        gameWinnerVar = "Congrats! You win the game with a score of " + playerScore + 
+        " vs " + computerScore + " to the computer.";
     } else {
-        console.log("You lost the game! The score was " + 
-        computerScore + " to " + humanScore);
+        gameWinnerVar = "You lost the game! Your score was " + playerScore + 
+        " vs " + computerScore + " to the computer.";
     }
-    restartGame();
+    let gameWinnerString = String(gameWinnerVar);
+    return gameWinnerString;
 };
 
 function restartGame() {
-    humanScore = 0;
+    playerScore = 0;
     computerScore = 0;
+    updateGameScoreBoard(playerScore, computerScore);
+    document.getElementById("playerChoice").innerText = "?";
+    document.getElementById("computerChoice").innerText = "?";
+    document.getElementById("introTextHeading").innerText = "Make your first choice";
+    document.getElementById("introTextBody").innerText = "First to 5 points wins" +
+    " - Good luck!";
+    endGameModal.style.display = "none";
 }
